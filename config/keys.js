5c77d9d027e20c69ff65c2203d738d9795d75abb
BishalRaj <bisal1shakya@gmail.com>
2020-12-28 23:15:14 +0545

    database setup for hosting

--- config/keys.js
+++ config/keys.js
@@ -4,6 +4,8 @@ module.exports = {
       "703118685870-ejuuhkd0hhr80i64smb8v7u3ed6300qc.apps.googleusercontent.com",
     clientSecret: "pZ6-MtS5RbxaCorf1fXwXIDY",
   },
-  MongoURI: "mongodb://localhost:27017/niharika",
+  // MongoURI: "mongodb://localhost:27017/niharika",
+  MongoURI:
+    "mongodb+srv://niharika:niharika@cluster0.ak9au.mongodb.net/niharika",
   secret: "secretkry",
 };
